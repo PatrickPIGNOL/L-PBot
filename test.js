083244e3b1d82b241030efb174857a2e67c84384
Patrick <patrick@Thor>
2020-04-12 02:44:13 +0200

    Update launch.json, config.json, and 8510 more files...

---
+++ test.js
@@ -0,0 +1,33 @@
+RichEmbedTest
+{
+    const embed = new Discord.RichEmbed()
+        .setTitle("This is your title, it can hold 256 characters")
+        .setAuthor("Author Name", "https://i.imgur.com/lm8s41J.png")
+        /*
+        * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
+        */
+        .setColor(0x00AE86)
+        .setDescription("This is the main body of text, it can hold 2048 characters.")
+        .setFooter("This is the footer text, it can hold 2048 characters", "http://i.imgur.com/w1vhFSR.png")
+        .setImage("http://i.imgur.com/yVpymuV.png")
+        .setThumbnail("http://i.imgur.com/p2qNFag.png")
+        /*
+        * Takes a Date object, defaults to current date.
+        */
+        .setTimestamp()
+        .setURL("https://discord.js.org/#/docs/main/indev/class/RichEmbed")
+        .addField("This is a field title, it can hold 256 characters",
+            "This is a field value, it can hold 1024 characters.")
+        /*
+        * Inline fields may not display as inline if the thumbnail and/or image is too big.
+        */
+        .addField("Inline Field", "They can also be inline.", true)
+        /*
+        * Blank field, useful to create some space.
+        */
+        .addBlankField(true)
+        .addField("Inline Field 3", "You can have a maximum of 25 fields.", true);
+        
+    message.channel.send({embed});
+  //https://gblobscdn.gitbook.com/assets%2F-LAEeOAJ8-CJPfZkGKqI%2F-Lh-d6Qc42Rq3BmspE9l%2F-LAEmPBF47FJgnfBD21P%2Fembedexample2.png?generation=1560157655002946&alt=media
+}
